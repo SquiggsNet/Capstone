088be463a8e8a61732d1624988a320ab4498ba35
squiggsnet <squiggs.rafael@gmail.com>
2017-01-18 20:07:18 -0400

    Crud on Users working with corresponding views

---
+++ MouseMaintenance/app/Http/Controllers/UserController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Http\Requests;
+use Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //talk to model
+        $users = User::all();
+        //pick view to display
+        return view('users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::create([
+            'first_name' => $request['first_name'],
+            'last_name' => $request['last_name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+            'student_id' => $request['student_id'],
+            'phone' => $request['phone']
+        ]);
+        $user->save();
+        return redirect()->action('UserController@index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //talk to model
+        $user = User::find($id);
+        //pick view to display
+        return view('users.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $users = User::find($id);
+        $users->first_name = $request['first_name'];
+        $users->last_name = $request['last_name'];
+        $users->email = $request['email'];
+        $users->password = Hash::make($request['password']);
+        $users->student_id = $request['student_id'];
+        $users->phone = $request['phone'];
+        $users->save();
+        return redirect()->action('UserController@index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $users = User::find($id);
+        $users->delete();
+        return redirect()->action('UserController@index');
+    }
+}

67241bcd8e861557c8b6527757a7c59c59feb936
squiggsnet <squiggs.rafael@gmail.com>
2017-01-18 20:01:44 -0400

    Crud on Users working with corresponding views

--- MouseMaintenance/resources/views/users/index.blade.php
+++ MouseMaintenance/resources/views/users/index.blade.php
@@ -2,34 +2,42 @@
 
 @section('content')
 <div class="container">
-    <h1>Privileges</h1>
+    <h1>Users</h1>
     <table class="table table-bordered table-striped">
         <thead>
         <tr>
             <th>ID</th>
-            <th>Name</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Student ID</th>
             <th></th>
             <th></th>
         </tr>
         </thead>
         <tbody>
-        @foreach ($privileges as $privilege)
+        @foreach ($users as $user)
         <tr>
             <td>
-                <a href="{{ action( 'PrivilegeController@show', ['id' => $privilege->id]) }}">
-                    {{$privilege->id}}
+                <a href="{{ action( 'UserController@show', ['id' => $user->id]) }}">
+                    {{$user->id}}
                 </a>
             </td>
-            <td>{{$privilege->name}}</td>
+            <td>{{$user->first_name}}</td>
+            <td>{{$user->last_name}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->phone}}</td>
+            <td>{{$user->student_id}}</td>
             <td>
-                {{ Form::open(['action' => ['PrivilegeController@edit', $privilege], 'method' => 'get']) }}
+                {{ Form::open(['action' => ['UserController@edit', $user], 'method' => 'get']) }}
                 <button type="submit" >
                     <span class="glyphicon glyphicon-pencil"></span>
                 </button>
                 {{ Form::close() }}
             </td>
             <td>
-                {{ Form::open(['action' => ['PrivilegeController@destroy', $privilege], 'method' => 'delete']) }}
+                {{ Form::open(['action' => ['UserController@destroy', $user], 'method' => 'delete']) }}
                 <button type="submit" >
                     <span class="glyphicon glyphicon-trash"></span>
                 </button>
@@ -39,8 +47,8 @@
         @endforeach
         </tbody>
     </table>
-    <a href="{{ action( 'PrivilegeController@create') }}">
-        Create a New Privelege
+    <a href="{{ action( 'UserController@create') }}">
+        Create a New User
     </a>
 </div>
 @endsection

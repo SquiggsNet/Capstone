02fb3fe94f90c85e4ede1a4b55f30e2249e6eafa
squiggsnet <squiggs.rafael@gmail.com>
2017-02-15 16:08:24 -0400

    modified colors

--- MouseMaintenance/resources/views/home.blade.php
+++ MouseMaintenance/resources/views/home.blade.php
@@ -21,25 +21,15 @@
 
                     <a href="{{ url('/storages') }}"><h3>Storage</h3></a>
 
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th>Storage Location</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($storages as $storage)
-                            <tr>
-                                <td>
-                                    <a href="{{ action( 'StorageController@show', ['id' => $storage->id]) }}">
-                                        {{$storage->type}} (Type should be changed from boolean to string
-                                        to represent freezer type(freezer 1, freezer 2, Histology)
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                    @foreach ($storages as $storage)
+
+                        <a class="btn btn-primary btn-lg btn-block" href="{{ action( 'StorageController@show', ['id' => $storage->id]) }}" role="button">
+                            {{--<a class="btn btn-primary btn-lg quarter colonyBtn" href="{{ action( 'ColonyController@show', ['id' => $colony->id]) }}" role="button">--}}
+                            <span class="glyphicon glyphicon-zoom-in" aria-hidden="true"></span> (Type should be changed from boolean to string
+                            to represent freezer type(freezer 1, freezer 2, Histology)
+                        </a>
+
+                    @endforeach
 
                     <a href="{{ url('/surgeries') }}"><h3>Surgeries</h3></a>
 
